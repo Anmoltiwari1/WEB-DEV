8aed80da8305104ff02d8a11802169e4a585aa9d
Anmoltiwari1 <124855487+Anmoltiwari1@users.noreply.github.com>
2025-05-24 22:34:29 +0530

    Add files via upload

---
+++ Javascript/part1/changes.js
@@ -0,0 +1,10 @@
+//This can be changed
+let gameName ="spiderman";
+gameName ="batman";
+console.log(gameName);
+
+//you cannot change the const.
+const username="anmol.com";
+//this will give error
+username="tiwari.com";
+
